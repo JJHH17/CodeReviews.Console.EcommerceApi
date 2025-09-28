9d1a61723ba05905c6417326be82c3e9f5c76e1c
James Hatfield <james.hatfield17@icloud.com>
2025-09-28 11:32:09 +0100

    Added reference to productService

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Program.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Program.cs
@@ -1,19 +1,17 @@
 using ECommerceApi.JJHH17.Data;
+using ECommerceApi.JJHH17.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<ProductsDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
