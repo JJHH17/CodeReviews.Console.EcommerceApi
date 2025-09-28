67f56b089d6de7cf49bff06da5cd1e25f90bf6cc
James Hatfield <james.hatfield17@icloud.com>
2025-09-28 11:09:03 +0100

    Added reference to product model

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerceApi.JJHH17.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceApi.JJHH17.Data
 {
     public class ProductsDbContext : DbContext
     {
         public ProductsDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
     }
 }
