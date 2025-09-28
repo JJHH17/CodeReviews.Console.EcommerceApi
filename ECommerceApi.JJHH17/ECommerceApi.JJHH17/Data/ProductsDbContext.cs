c4f807b3eff820ef35e2d23745a31783b96a3920
James Hatfield <james.hatfield17@icloud.com>
2025-09-28 10:56:13 +0100

    Added dbContext

---
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceApi.JJHH17.Data
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions options) : base(options) { }
+    }
+}
