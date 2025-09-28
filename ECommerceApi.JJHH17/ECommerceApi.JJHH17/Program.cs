83ee73c6f72ee5eb91f45aebc9c4b533a44bc9e1
James Hatfield <james.hatfield17@icloud.com>
2025-09-28 11:05:08 +0100

    Added reference to connection string

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Program.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Program.cs
@@ -1,9 +1,15 @@
+using ECommerceApi.JJHH17.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ProductsDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -13,5 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapControllers();
 
 app.Run();
