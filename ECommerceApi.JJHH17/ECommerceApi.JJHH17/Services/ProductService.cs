21f70568fa3ef22eb8a212667563b1f01d2c38e1
James Hatfield <james.hatfield17@icloud.com>
2025-09-28 11:39:21 +0100

    Created product service methods

---
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using ECommerceApi.JJHH17.Data;
+using ECommerceApi.JJHH17.Models;
+
+namespace ECommerceApi.JJHH17.Services
+{
+    public interface IProductService
+    {
+        public List<Product> GetAllProducts();
+        public Product? GetProductById(int id);
+        public Product CreateProduct(Product product);
+        public Product UpdateProduct(int id, Product product);
+        public string? DeleteProduct(int id);
+    }
+
+    public class ProductService : IProductService
+    {
+        private readonly ProductsDbContext _dbContext;
+
+        public ProductService(ProductsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Product CreateProduct(Product product)
+        {
+            var savedProduct = _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+            return savedProduct.Entity;
+        }
+
+        public string? DeleteProduct(int id)
+        {
+            Product savedProduct = _dbContext.Products.Find(id);
+
+            if (savedProduct == null) { return null; }
+
+            _dbContext.Products.Remove(savedProduct);
+            _dbContext.SaveChanges();
+
+            return $"Successfully deleted product with ID: {id}";
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            Product savedProduct = _dbContext.Products.Find(id);
+            return savedProduct;
+        }
+
+        public Product UpdateProduct(int id, Product product)
+        {
+            Product savedProduct = _dbContext.Products.Find(id);
+
+            if (savedProduct == null) { return null; }
+
+            _dbContext.Entry(savedProduct).CurrentValues.SetValues(product);
+            _dbContext.SaveChanges();
+
+            return savedProduct;
+        }
+    }
+}
