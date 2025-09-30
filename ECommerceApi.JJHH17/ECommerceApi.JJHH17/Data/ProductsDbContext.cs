1906d214e60afbb5da1b1d9fc35451ab28a32a8a
James Hatfield <james.hatfield17@icloud.com>
2025-09-30 22:19:26 +0100

    Created products relationship with categories

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
@@ -6,11 +6,18 @@ namespace ECommerceApi.JJHH17.Data
 {
     public class ProductsDbContext : DbContext
     {
-        public ProductsDbContext(DbContextOptions options) : base(options)
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<Product>()
+                .HasOne(e => e.Category)
+                .WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId)
+                .IsRequired();
         }
-
-        public DbSet<Product> Products { get; set; }
     }
 }
