9c6ea8bf22c34a05671984e1c18eb9539cfd6275
James Hatfield <james.hatfield17@icloud.com>
2025-09-30 22:19:44 +0100

    Created model for product which feeds to categories

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Models/Product.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Models/Product.cs
@@ -5,5 +5,8 @@
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
     }
 }
