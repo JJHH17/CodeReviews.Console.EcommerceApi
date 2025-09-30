a9010d262dc8cbbff5bc5d978e058873d2b0aa92
James Hatfield <james.hatfield17@icloud.com>
2025-09-30 21:42:51 +0100

    Created db context for products

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Data/ProductsDbContext.cs
@@ -1,12 +1,16 @@
 ﻿using ECommerceApi.JJHH17.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace ECommerceApi.JJHH17.Data
 {
     public class ProductsDbContext : DbContext
     {
-        public ProductsDbContext(DbContextOptions options) : base(options) { }
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
 
-    public DbSet<Product> Products { get; set; }
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
