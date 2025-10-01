b10df847866a82583ff7fd3ada0c244e722e9ee4
James Hatfield <james.hatfield17@icloud.com>
2025-10-01 20:21:21 +0100

    Created categories controller class

---
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceApi.JJHH17.Models;
+using ECommerceApi.JJHH17.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApi.JJHH17.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    // Example call: http://localhost:5609/api/category/
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Category>> GetAllCategories()
+        {
+            return Ok(_categoryService.GetAllCategories());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Category> GetCategoryById(int id)
+        {
+            var result = _categoryService.GetCategoryById(id);
+
+            if (result == null) { return NotFound(); }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<Category> CreateCategory(Category category)
+        {
+            return Ok(_categoryService.CreateCategory(category));
+        }
+    }
+}
