27e7581bee417c379a610ac7b832f203c389a03d
James Hatfield <james.hatfield17@icloud.com>
2025-10-03 13:41:30 +0100

    Created Sale model

---
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Models/Sale.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceApi.JJHH17.Models
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public List<Product> Products { get; } = [];
+        public decimal SalePrice => Products.Sum(p => p.Price);
+        public int ItemCount => Products.Count();
+    }
+}
