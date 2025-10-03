11a2e6912a72086ab94e698ec4c47bd7aeb3c2c6
James Hatfield <james.hatfield17@icloud.com>
2025-10-03 21:24:04 +0100

    Created many to many sales model

--- ECommerceApi.JJHH17/ECommerceApi.JJHH17/Models/Sale.cs
+++ ECommerceApi.JJHH17/ECommerceApi.JJHH17/Models/Sale.cs
@@ -7,4 +7,8 @@
         public decimal SalePrice => Products.Sum(p => p.Price);
         public int ItemCount => Products.Count();
     }
+
+    public record CreateSaleDto(List<int> ProductIds);
+
+    public record SaleWithProductsDto(int SaleId, int ItemCount, decimal SalePrice, List<ProductDto> Products);
 }
